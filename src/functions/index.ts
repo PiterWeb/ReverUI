7fb7e3a624cc31e7dc5d43f7258252d33992b374
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2026-04-13 21:39:11 +0200

    fix Signal type import/export

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,2 +1,2 @@
 export { $useEffect as $useEffect__internal, $useSignal as $useSignal__internal, $useDerived as $useDerived__internal } from "./UIFunctions_internal"
-export {$useEffect, $useSignal, $useDerived ,Signal} from "./UIFunctions"
+export {$useEffect, $useSignal, $useDerived , type Signal} from "./UIFunctions"
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "./types/index";
 import "./UIElements"
 
-export { ReverVitePlugin, ReverVitePluginOptions } from "./vite";
-export { $useEffect, $useSignal, $useDerived, $useEffect__internal, $useSignal__internal, $useDerived__internal, Signal } from "./functions/index";
+export { ReverVitePlugin, type ReverVitePluginOptions } from "./vite";
+export { $useEffect, $useSignal, $useDerived, $useEffect__internal, $useSignal__internal, $useDerived__internal, type Signal } from "./functions/index";
 export { $UI, default as UI } from "./UI";
 export { $Component, $Show, $For } from "./components"
